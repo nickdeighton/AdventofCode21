487842e527f4a5caa333466f77c8fe18d4963bdc
nickdeighton <nick.deighton@gmail.com>
2021-12-20 23:30:31 -0700

    Day 1 Challenge Complete

---
+++ SonarSweep.py
@@ -0,0 +1,29 @@
+
+with open("input_day1") as f:
+    lines = f.readlines()
+
+count = 0;
+prevSum = int(0);
+currSum = int(0);
+
+for line in range(len(lines) - 2):
+    
+    # currSum = int(line)
+    # if line > previous:
+    #     count += 1
+        
+    # previous = line
+    
+    window_vals = lines[line: line + 3]
+    
+    for x in window_vals:
+        currSum += int(x)
+        
+    if prevSum > 0 and currSum > prevSum:
+        count += 1;
+    
+    prevSum = currSum
+    currSum = 0
+    
+print(count)
+
